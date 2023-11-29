e966ba4601154219ad3218a4db6803a4e6bf7744
saintskeeper <walterdayjr@gmail.com>
2023-11-28 20:49:18 -0500

    adding test namespace

---
+++ scripts/bootstrap.sh
@@ -0,0 +1,7 @@
+flux bootstrap github \
+  --owner=$GITUSER \
+  --repository=$GITUSER/CloudMasnonThrowAway01 \
+  --path=clusters/dev-cluster \
+  --personal \
+  --branch=main \
+  --network-policy=false
